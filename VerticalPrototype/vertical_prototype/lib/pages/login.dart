cf5a7ec14e53afd916190581c121ee44594bf072
Marcel Medeiros <marcel_segundo@outlook.com>
2024-03-19 09:25:59 +0000

    Fazendo pagina de login - vertical prot

--- VerticalPrototype/vertical_prototype/lib/pages/login.dart
+++ VerticalPrototype/vertical_prototype/lib/pages/login.dart
@@ -23,15 +23,39 @@ class LoginPage extends StatelessWidget {
             SizedBox(height: 70,),
             //some text
             Text(
-              'Hey! We are Martechplace, You are welcome here!',
+              'Hey! We are Martechplace, You are welcome hre!',
               style: TextStyle(
                 fontSize: 16,
               ) ,
             ),
+            
+            SizedBox(height: 50,),
             //username textfield
-          
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+                  fillColor: Colors.white, filled:true,
+                  hintText: "Username"
+                ),
+              ),
+            ),
+
+            SizedBox(height: 25,),
             //password textfield
-          
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+                  fillColor: Colors.white, filled:true,
+                  hintText: "Password"
+                ),
+              ),
+            )          
             //forgot password?
           
             //sign in button
