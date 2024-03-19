37271bdbc30f3a91cbf8cf4ab1f232918ca2393a
Marcel Medeiros <you@example.com>
2024-03-19 20:02:19 +0000

    Pagina de login em progresso

--- VerticalPrototype/vertical_prototype/lib/pages/login.dart
+++ VerticalPrototype/vertical_prototype/lib/pages/login.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -7,26 +9,26 @@ class LoginPage extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Color.fromARGB(226, 220, 233, 241),
       body: SafeArea(
         child: Center(
           child: Column(children: [
             SizedBox(height: 70),
-
             //Icon
             Icon(
               Icons.lock,
               size: 100,
             ),
 
+
             SizedBox(height: 70,),
             //some text
             Text(
-              'Hey! We are Martechplace, You are welcome hre!',
-              style: TextStyle(
-                fontSize: 16,
-              ) ,
+              'Hey! We are Martechplace, You are welcome here!',
+              style: GoogleFonts.amaranth(  
+                fontSize: 25,
+              ),
             ),
             
             SizedBox(height: 50,),
@@ -38,7 +40,8 @@ class LoginPage extends StatelessWidget {
                   enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
                   focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
                   fillColor: Colors.white, filled:true,
-                  hintText: "Username"
+                  hintText: "Username",
+                  hintStyle: TextStyle(color: Colors.grey[500]),
                 ),
               ),
             ),
@@ -52,12 +55,29 @@ class LoginPage extends StatelessWidget {
                   enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
                   focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
                   fillColor: Colors.white, filled:true,
-                  hintText: "Password"
+                  hintText: "Password",
+                  hintStyle: TextStyle(color: Colors.grey[500]),
                 ),
+                obscureText: true,
               ),
-            )          
+            ),
+
+            SizedBox(height: 10,),
             //forgot password?
-          
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Forgot password?',
+                    style: GoogleFonts.amaranth(  
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
             //sign in button
           
             // or continue with: google
